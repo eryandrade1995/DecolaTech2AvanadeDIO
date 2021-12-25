66e2b4d7abc0fe4ae7ef07ea2faf11ae60f411af
eryandrade1995 <erywelton.2014@gmail.com>
2021-12-25 00:16:24 -0300

    up

---
+++ TestVSGitHub/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = "Hello ";
+            var y = "World";
+            var z = "!";
+            Console.WriteLine(x + y + z);
+        }
+    }
+}
