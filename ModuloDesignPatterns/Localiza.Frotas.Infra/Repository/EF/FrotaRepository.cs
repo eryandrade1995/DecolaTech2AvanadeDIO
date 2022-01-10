d01a2dade10dd46b3bfaa8c949a13804356d20a4
eryandrade1995 <erywelton.2014@gmail.com>
2022-01-10 13:51:34 -0300

    Acompanhamento/finalização(Codando) o módulo de Design Patterns

---
+++ ModuloDesignPatterns/Localiza.Frotas.Infra/Repository/EF/FrotaRepository.cs
@@ -0,0 +1,35 @@
+﻿using Localiza.Frotas.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Localiza.Frotas.Infra.Repository.EF
+{
+    public class FrotaRepository : IVeiculoRepository
+    {
+        private readonly FrotaContext dbContext;
+        public FrotaRepository(FrotaContext dbContext) => this.dbContext = dbContext;
+        public void Add(Veiculo veiculo)
+        {
+            dbContext.veiculos.Add(veiculo);
+            dbContext.SaveChanges();
+        }
+
+        public void Delete(Veiculo veiculo)
+        {
+            dbContext.veiculos.Remove(veiculo);
+            dbContext.SaveChanges();
+        }
+
+        public IEnumerable<Veiculo> GetAll() => dbContext.veiculos.ToListAsync().Result;
+
+        public Veiculo GetById(Guid id) => dbContext.veiculos.SingleOrDefaultAsync().Result;
+
+        public void Update(Veiculo veiculo)
+        {
+            dbContext.Entry(veiculo).State = EntityState.Modified;
+            dbContext.SaveChanges();
+        }
+    }
+}
---
+++ ModuloDesignPatterns/Localiza.Frotas.Infra/Singleton/SingletonContainer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Localiza.Frotas.Infra.Singleton
+{
+    public class SingletonContainer
+    {
+
+        public Guid Id { get; } = Guid.NewGuid();
+
+    }
+}
